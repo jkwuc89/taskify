2325f9cb9a7e72d1f403982e26499440debe33b0
Keith Wedinger <keith@emporatitle.com>
2023-01-12 15:29:56 -0500

    Create new tasks and show task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def index
     @tasks = Task.all
+    @task = Task.new
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
