bd1d85f7162cf1c75238657ceb24600ba7a7459f
Keith Wedinger <keith@emporatitle.com>
2023-01-13 10:05:28 -0500

    Add completed checkbox, edit button and delete button This includes underlying controller changes to implement the interactions.ˆ

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# README
+# Let's Learn Ruby on Rails + Hotwire by Building a To-Do List
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+See https://www.akshaykhot.com/building-to-do-list-using-hotwire-and-stimulus/
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,9 +12,37 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url, notice: "Task was successfully deleted."
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
   def index
     @tasks = Task.all
     @task = Task.new
+  end
+
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: params[:completed])
+
+    render json: { message: "Success" }
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
